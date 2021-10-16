5387a3410ac739d98bba8e7352645129cf110ef9
Kat Marchán <kzm@zkat.tech>
2021-10-15 19:31:20 -0700

    wip

--- lib/banchan/uploads/uploads.ex
+++ lib/banchan/uploads/uploads.ex
@@ -1,5 +1,6 @@
 defmodule Banchan.Uploads do
-  def sign_s3_upload_url() do
-  
+  def sign_s3_upload_url(bucket, path) do
+    ExAws.Config.new(:s3)
+    |> ExAws.S3.presigned_url(:put, bucket, path, )
   end
 end
