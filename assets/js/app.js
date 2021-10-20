7052bce2a690e8910c865501b8ff650ba4457845
Kat Marchán <kzm@zkat.tech>
2021-10-19 19:29:32 -0700

    add js-side uploader for direct-to-s3 uploads

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,8 +5,38 @@ import topbar from "topbar"
 import Hooks from "./_hooks"
 import { LiveSocket } from "phoenix_live_view"
 
+let Uploaders = {}
+
+Uploaders.S3 = function (entries, onViewError) {
+  entries.forEach(entry => {
+    let formData = new FormData()
+    let { url, fields } = entry.meta
+    Object.entries(fields).forEach(([key, val]) => formData.append(key, val))
+    formData.append("file", entry.file)
+    let xhr = new XMLHttpRequest()
+    onViewError(() => xhr.abort())
+    xhr.onload = () => xhr.status === 204 ? entry.progress(100) : entry.error()
+    xhr.onerror = () => entry.error()
+    xhr.upload.addEventListener("progress", (event) => {
+      if (event.lengthComputable) {
+        let percent = Math.round((event.loaded / event.total) * 100)
+        if (percent < 100) { entry.progress(percent) }
+      }
+    })
+
+    xhr.open("POST", url, true)
+    xhr.send(formData)
+  })
+}
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, { hooks: Hooks, params: { _csrf_token: csrfToken } })
+let liveSocket = new LiveSocket("/live", Socket, {
+  hooks: Hooks,
+  params: {
+    _csrf_token: csrfToken
+  },
+  uploaders: Uploaders
+})
 
 window.Alpine = Alpine
 Alpine.start()
