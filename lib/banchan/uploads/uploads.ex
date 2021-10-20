ea56e5de756a47c4509c64b5839f1eabf3f1f737
Kat Marchán <kzm@zkat.tech>
2021-10-19 21:12:59 -0700

    fmt

--- lib/banchan/uploads/uploads.ex
+++ lib/banchan/uploads/uploads.ex
@@ -9,19 +9,25 @@ defmodule Banchan.Uploads do
     config = ExAws.Config.new(:s3)
     bucket = Application.fetch_env!(:ex_aws, :bucket)
 
-    {:ok, fields} = SimpleS3Upload.sign_form_upload(
-      config,
-      bucket,
-      [key: path] ++
-        opts ++
-        [
-          max_file_size: 12_000_000,
-          content_type: "application/octet-stream",
-          expires_in: :timer.minutes(15)
-        ]
-    )
+    {:ok, fields} =
+      SimpleS3Upload.sign_form_upload(
+        config,
+        bucket,
+        [key: path] ++
+          opts ++
+          [
+            max_file_size: 12_000_000,
+            content_type: "application/octet-stream",
+            expires_in: :timer.minutes(15)
+          ]
+      )
 
-    meta = %{uploader: "S3", key: path, url: "http://#{bucket}.s3-#{config.region}.amazonaws.com", fields: fields}
+    meta = %{
+      uploader: "S3",
+      key: path,
+      url: "http://#{bucket}.s3-#{config.region}.amazonaws.com",
+      fields: fields
+    }
 
     {:ok, meta}
   end
